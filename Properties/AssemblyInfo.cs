66cf3d12d0d16053966dd22965549bd6aeba1af0
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2021-12-13 21:14:47 +0100

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: Guid("ea24337b-9227-4c97-b6da-b21147dae666")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(true)]
+[assembly: AssemblyKeyName("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTitle("LunaTokenGenV2")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LunaTokenGenV2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
