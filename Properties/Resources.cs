b88c6d29da2c080e56a0ec8cb4cdb954dc3656c7
Cabbo <92642446+CabboShiba@users.noreply.github.com>
2021-12-13 21:15:01 +0100

    Add files via upload

---
+++ Properties/Resources.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace LunaTokenGenV2.Properties
+{
+	// Token: 0x02000031 RID: 49
+	[CompilerGenerated]
+	[DebuggerNonUserCode]
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+	internal class Resources
+	{
+		// Token: 0x0600013B RID: 315 RVA: 0x000022E9 File Offset: 0x000004E9
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000021 RID: 33
+		// (get) Token: 0x0600013C RID: 316 RVA: 0x000148A0 File Offset: 0x00012AA0
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (Resources.resourceMan == null)
+				{
+					ResourceManager resourceManager = new ResourceManager("LunaTokenGenV2.Properties.Resources", typeof(Resources).Assembly);
+					Resources.resourceMan = resourceManager;
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000022 RID: 34
+		// (get) Token: 0x0600013D RID: 317 RVA: 0x000148E0 File Offset: 0x00012AE0
+		// (set) Token: 0x0600013E RID: 318 RVA: 0x0000280D File Offset: 0x00000A0D
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x17000023 RID: 35
+		// (get) Token: 0x0600013F RID: 319 RVA: 0x000148F4 File Offset: 0x00012AF4
+		internal static string d
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("d", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x0400018D RID: 397
+		private static ResourceManager resourceMan;
+
+		// Token: 0x0400018E RID: 398
+		private static CultureInfo resourceCulture;
+	}
+}
---
+++ Properties/Settings.Designer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace LunaTokenGenV2.Properties
+{
+	// Token: 0x02000032 RID: 50
+	[CompilerGenerated]
+	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+	internal sealed partial class Settings : ApplicationSettingsBase
+	{
+		// Token: 0x17000024 RID: 36
+		// (get) Token: 0x06000140 RID: 320 RVA: 0x00014918 File Offset: 0x00012B18
+		public static Settings Default
+		{
+			get
+			{
+				return Settings.defaultInstance;
+			}
+		}
+
+		// Token: 0x17000025 RID: 37
+		// (get) Token: 0x06000141 RID: 321 RVA: 0x00002815 File Offset: 0x00000A15
+		// (set) Token: 0x06000142 RID: 322 RVA: 0x00002827 File Offset: 0x00000A27
+		[DefaultSettingValue("True")]
+		[UserScopedSetting]
+		[DebuggerNonUserCode]
+		public bool Topmost1
+		{
+			get
+			{
+				return (bool)this["Topmost1"];
+			}
+			set
+			{
+				this["Topmost1"] = value;
+			}
+		}
+
+		// Token: 0x17000026 RID: 38
+		// (get) Token: 0x06000143 RID: 323 RVA: 0x0000283A File Offset: 0x00000A3A
+		// (set) Token: 0x06000144 RID: 324 RVA: 0x0000284C File Offset: 0x00000A4C
+		[UserScopedSetting]
+		[DebuggerNonUserCode]
+		[DefaultSettingValue("True")]
+		public bool TopmostSetting
+		{
+			get
+			{
+				return (bool)this["TopmostSetting"];
+			}
+			set
+			{
+				this["TopmostSetting"] = value;
+			}
+		}
+
+		// Token: 0x17000027 RID: 39
+		// (get) Token: 0x06000145 RID: 325 RVA: 0x0001492C File Offset: 0x00012B2C
+		// (set) Token: 0x06000146 RID: 326 RVA: 0x0000285F File Offset: 0x00000A5F
+		[DefaultSettingValue("")]
+		[DebuggerNonUserCode]
+		[UserScopedSetting]
+		public string Totalgenlog
+		{
+			get
+			{
+				return (string)this["Totalgenlog"];
+			}
+			set
+			{
+				this["Totalgenlog"] = value;
+			}
+		}
+
+		// Token: 0x17000028 RID: 40
+		// (get) Token: 0x06000147 RID: 327 RVA: 0x0001494C File Offset: 0x00012B4C
+		// (set) Token: 0x06000148 RID: 328 RVA: 0x0000286D File Offset: 0x00000A6D
+		[DefaultSettingValue("")]
+		[UserScopedSetting]
+		[DebuggerNonUserCode]
+		public string Validgentoken
+		{
+			get
+			{
+				return (string)this["Validgentoken"];
+			}
+			set
+			{
+				this["Validgentoken"] = value;
+			}
+		}
+
+		// Token: 0x0400018F RID: 399
+		private static Settings defaultInstance = (Settings)SettingsBase.Synchronized(new Settings());
+	}
+}
